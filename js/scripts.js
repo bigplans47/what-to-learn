23cee707f6adc3dc55ee9607df4c1a11b52292c6
Guest User <Guest@Epicodus-9E.local>
2017-08-11 10:52:49 -0700

    add variables to javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 // back end
 
-var question1 = $("input:radio[name=question1]:checked").val();
-var question2 = $("input:radio[name=question2]:checked").val();
-var question3 = $("input:radio[name=question3]:checked").val();
-var question4 = $("input:radio[name=question4]:checked").val();
-var question5 = $("input:radio[name=question5]:checked").val();
-var Name = $("#name").val();
+var xx = $("input:radio[name=question1]:checked").val();
+
 
 // front end
 $(document).ready(function() {
   $("form#generalinfo").submit(function(event) {
     event.preventDefault();
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var Name = $("#name").val();
+    var total = question1 + question2 + question3 + question4 + question5
     $("#output").text(Name);
   });
 });
