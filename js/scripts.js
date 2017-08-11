fe5f98b15627ea88a60c658bc7b72f1dced181ab
Dan Kiss <bigplans47@gmail.com>
2017-08-11 16:50:10 -0700

    add bootstrap styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,8 @@
 $(document).ready(function() {
-
-
-
   $("form#generalquestionsforcode").submit(function(event) {
     event.preventDefault();
-
-    // $(".javaresult").hide();
-    // $(".phpresult").hide();
-    // $(".rubyresult").hide();
-
     var question1 = $("input:radio[name=question1]:checked").val();
     var question2 = $("input:radio[name=question2]:checked").val();
-    // debugger;
     var question3 = $("input:radio[name=question3]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
     var question5 = $("input:radio[name=question5]:checked").val();
@@ -23,33 +14,36 @@ $(document).ready(function() {
     var qtotal = q1+q2+q3+q4+q5;
     var Name = $("#name").val();
     var total = question1 + question2 + question3 + question4 + question5;
-    // $("#output").text(Name);
     $("#output").text(Name+" here is your result:");
-    // $("#output").text(question2);
-    // $("#output").text(question3);
-    // $("#output").text(question4);
-    // $("#output").text(question5);
-    // $("#output").text(question6);
-    // $("#output").text(total);
-
-
-
     if (qtotal <= 6) {
       $(".radio").toggle();
       $("h1").toggle();
       $("h5").toggle();
       $(".whatsname").toggle();
-      // $("h1").toggle();
-      // $("#output").text(Name+" here is your result:");
+      $(".btn").toggle();
+      $(".jumbotron").toggle();
+      $(".well").toggle();
       $('.javaresult').fadeToggle();
     } else if (qtotal >= 7 && qtotal <= 9) {
+      $(".radio").toggle();
+      $("h1").toggle();
+      $("h5").toggle();
+      $(".whatsname").toggle();
+      $(".btn").toggle();
+      $(".jumbotron").toggle();
+      $(".well").toggle();
       $(".phpresult").fadeToggle();
     } else if (qtotal >=10 && qtotal <=15) {
+      $(".radio").toggle();
+      $("h1").toggle();
+      $("h5").toggle();
+      $(".whatsname").toggle();
+      $(".btn").toggle();
+      $(".jumbotron").toggle();
+      $(".well").toggle();
       $(".rubyresult").fadeToggle();
     } else {
       alert("Not all questions answered, refresh page and answer all before submitting");
     }
-
-    // note add an if statement to do alert if answers add to less then 6 as question was not answered
   });
 });
