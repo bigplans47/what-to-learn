47761cd5c6fc3ac4638dfd443c7377e8b4d683db
Guest User <Guest@Epicodus-9E.local>
2017-08-11 13:12:25 -0700

    add images

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,27 @@
-// back end
-
-var xx = $("input:radio[name=question1]:checked").val();
-
-
-// front end
 $(document).ready(function() {
-  $("form#generalinfo").submit(function(event) {
+  $(".javaresult").hide();
+  $(".phpresult").hide();
+  $(".rubyresult").hide();
+
+
+  $("form#generalquestionsforcode").submit(function(event) {
     event.preventDefault();
+
     var question1 = parseInt($("input:radio[name=question1]:checked").val());
     var question2 = parseInt($("input:radio[name=question2]:checked").val());
     var question3 = parseInt($("input:radio[name=question3]:checked").val());
     var question4 = parseInt($("input:radio[name=question4]:checked").val());
     var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var question6 = parseInt($("input:radio[name=question6]:checked").val());
     var Name = $("#name").val();
-    var total = question1 + question2 + question3 + question4 + question5
+    var total = question1 + question2 + question3 + question4 + question5;
     $("#output").text(Name);
+
+
+    if (total >=1) {
+      $("form#generalquestionsforcode").hide();
+      $(".rubyresult").toggle();
+    };
+    // note add an if statement to do alert if answers add to less then 6 as question was not answered
   });
 });
